5e0a4ed255c72dcba15bf8bb5629fee517ecc261
ViniGhiraldi <vinighiraldi198@gmail.com>
2023-06-08 21:02:34 -0300

    feat: added cors config

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import 'dotenv/config';
 import { routes } from './routes';
+import cors from 'cors';
 
 const Server = express();
 
+Server.use(cors({
+    origin: process.env.CORS_ACCESS?.split(' ') || []
+}))
+
 Server.use(express.json());
+
 Server.use(routes);
 
 
